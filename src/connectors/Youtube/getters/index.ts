276ac9d8eb0d9440da6c95c2be2e4d63bfe4ac56
Wigger Boelens <me@wiggerboelens.com>
2023-05-11 15:29:58 +0200

    Remove option to get video info from chapter as it didn't really work
    Signed-off-by: Wigger Boelens <me@wiggerboelens.com>

--- src/connectors/Youtube/getters/index.ts
+++ src/connectors/Youtube/getters/index.ts
@@ -1,6 +1,5 @@
 import fromTitle from './fromTitle'
 import fromDescription from './fromDescription'
 import fromYoutubeMusic from './fromYoutubeMusic'
-import fromChapter from './fromChapter'
 
-export default [fromTitle, fromDescription, fromYoutubeMusic, fromChapter]
+export default [fromTitle, fromDescription, fromYoutubeMusic]
